90b061864637c665a5a964b49c4ee848d650767e
iBorisAlexander <tobarmirandaborisalexander@gmail.com>
2022-10-11 15:49:40 -0300

    comienzo del mapbox

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,9 @@ if (environment.production) {
   enableProdMode();
 }
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiaXdpaWR5eCIsImEiOiJjbDh3YTd4cTQwbHM1M3dzcmUyb29rY24wIn0.0k_yYJ_gDjFhdKDLQIlvCw';
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
